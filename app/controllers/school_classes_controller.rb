d1d0d057f5126d95c8cd4efd411d870a061bdba5
Edward Heaver <heaver.edward02@gmail.com>
2020-02-02 17:38:11 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,31 @@
+class SchoolClassesController < ApplicationController
+  
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+  
+  def new
+    @school_class = SchoolClass.new
+  end
+  
+  def create
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+  
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
+    
+    
+  private
+  
+  def school_class_params(*args)
+    params.require(:schoolclass).permit(*args)
+  end
+
+  
+end
--- app/controllers/school_classess_controller.rb
+++
@@ -1,4 +0,0 @@
-class SchoolClassesController < ApplicationController
-  
-  
-end
