03723070e6246c036923c94283e56d5cc7a89e65
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-02-02 02:36:13 -0500

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,41 @@
+class SchoolClassesController < ApplicationController
+  def index
+    @school_classes = SchoolClass.all
+  end
+  
+  def show
+    set_school_class
+  end
+  
+  def new 
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def edit
+    set_school_class
+  end
+
+  def update
+    set_school_class
+    #attr now nested within :school_class_class hash
+    # must be conscious of which attr to update, hence `.permit`
+    @school_class.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)        
+  end
+  
+  private
+  
+  def set_school_class
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
+  end
+end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,41 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+  
+  def show
+    set_student
+  end
+  
+  def new 
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    set_student
+  end
+
+  def update
+    set_student
+    #attr now nested within :student hash
+    # must be conscious of which attr to update, hence `.permit`
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)        
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
+end
