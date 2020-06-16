6f05784b09840b7977f18ab35f3953a9e6293bbc
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-16 06:27:58 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,22 +1,20 @@
 class StudentsController < ApplicationController
-  def new
-    @students = Student.new
-  end
-
-  def create
-    @student = Student.new
-    @student.first_name
-    @student.last_name
-    @student.save
-    redirect_to student_path(@student)
-  end
-
   def show
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+  end
+
   def edit
     @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def update
@@ -24,10 +22,10 @@ class StudentsController < ApplicationController
     @student.update(student_params)
     redirect_to student_path(@student)
   end
-  
+
   private
 
   def student_params
-    params.require(:student).permit(:first_name, :last_name)
+    params.require(:student).permit!
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,4 @@
 Rails.application.routes.draw do
-  resources :students, only: [:new, :create, :show, :edit, :update]
-  resources :school_classes, only: [:new, :create, :show, :edit, :update]
-  get 'students/new', to: 'students#new'
-
-  get 'students/create', to: 'students#create'
-
-  get 'students/:id/show', to: 'students#show'
-
-  get 'students/:id/edit', to: 'students#edit'
-
-  get 'students/:id/update', to: 'students#update'
-
-  get 'school_classes/new', to: 'school_classes#new'
-
-  get 'school_classes/create', to: 'school_classes#create'
-
-  get 'school_classes/:id/show', to: 'school_classes#show'
-
-  get 'school_classes/:id/edit', to: 'school_classes#edit'
-
-  get 'school_classes/:id/update', to: 'school_classes#update'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, except: [:destroy, :index]
+  resources :school_classes, except: [:destroy, :index]
 end
